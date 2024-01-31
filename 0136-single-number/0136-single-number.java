a7a9592e6d281bbc1b5e8a29dbee4b141f561313
vedant joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2024-02-01 04:11:36 +0530

    Time: 313 ms (8.01%), Space: 46.1 MB (31.03%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,35 +1,35 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
+        for(int i=0; i<nums.length; i++){
+            boolean flag = false;
+            for(int j=0; j<nums.length; j++){
+                if(i!=j && nums[i]==nums[j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag == false){
+                return nums[i];
+            }
+        }
+        return 0;
+        
+        // Map<Integer, Integer> map = new HashMap<>();// value and count
         // for(int i=0; i<nums.length; i++){
-        //     boolean flag = false;
-        //     for(int j=0; j<nums.length; j++){
-        //         if(i!=j && nums[i]==nums[j]){
-        //             flag = true;
-        //             break;
-        //         }
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i], map.get(nums[i])+1);
         //     }
-        //     if(flag == false){
-        //         return nums[i];
+        //     else{
+        //         map.put(nums[i], 1);
         //     }
         // }
-        // return 0;
-        
-        Map<Integer, Integer> map = new HashMap<>();// value and count
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i], 1);
-            }
-        }
-        for(Integer ele: map.keySet()){
-            if(map.get(ele)==1){
-                return ele;
-            }
-        }
-        return -1;
+        // for(Integer ele: map.keySet()){
+        //     if(map.get(ele)==1){
+        //         return ele;
+        //     }
+        // }
+        // return -1;
         
         
     }
