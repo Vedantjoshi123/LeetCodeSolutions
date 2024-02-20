ce95ea2b5c2e75aa0dd0280d4902d707f9bc0110
vedant joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2024-02-20 17:56:58 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (37.71%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,11 +1,41 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        for(int i=arr.length-1; i>=0; i--){
-            if(!arr[i].isEmpty()){
-                return arr[i].length();
+        // String[] arr = s.split(" ");
+        // for(int i=arr.length-1; i>=0; i--){
+        //     if(!arr[i].isEmpty()){
+        //         return arr[i].length();
+        //     }
+        // }
+        // return 0;
+        
+        int j = s.length()-1;
+        while(s.charAt(j) == ' '){
+            j--;
+        }
+        int count = 0;
+        for(int i = j; i>=0; i--){
+            if(s.charAt(i) != ' ' ){
+                count++;
+            }
+            else{
+                break;
             }
         }
-        return 0;
+        return count;
     }
 }
+//         int counter = 0;
+
+//         int j = s.length()-1;
+//         while(s.charAt(j) == ' ') {
+//             j--;
+//         }
+//         for(int i = j; i >= 0; i--) {
+//             if(s.charAt(i) != ' ') {
+//                 counter++;
+//             }else{
+//                 break;
+//             }
+//         }
+//         return counter;
+//     }
