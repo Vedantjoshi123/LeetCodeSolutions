3321ee8ca0fa300d6957ee7c5cda6c6091828fc5
vedant joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2024-02-21 17:59:13 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (89.46%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,58 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k= nums1.length - 1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+        while(i>=0){
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+        
+        //         int i = m-1;
+        // int j = n-1;
+        // int idx = nums1.length-1;
+        // //start looping from last values of both arrays
+        // //if elements remains in both, compare them and put larger
+        // while(i>=0 && j>=0){
+        //     if(nums1[i]> nums2[j]){
+        //         nums1[idx] = nums1[i];
+        //         i--;
+        //     }
+        //     else{
+        //         nums1[idx] = nums2[j];
+        //         j--;
+        //     }
+        //     idx--;
+        // }
+        // //if elements are remaining in one array, fill them
+        // while(i>=0){
+        //     nums1[idx] = nums1[i];
+        //     i--;
+        //     idx--;
+        // }
+        // while(j>=0){
+        //     nums1[idx] = nums2[j];
+        //     j--;
+        //     idx--;
+        // }
+        
+    }
+}
