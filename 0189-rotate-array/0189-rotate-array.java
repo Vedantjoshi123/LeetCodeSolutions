4c64c7f3821e42d66cd0786dd1ddb66703d3a715
vedant joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2024-06-21 11:46:59 +0530

    Time: 2 ms (29.43%), Space: 57.6 MB (18.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,53 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-         k = k % nums.length;
+        // Method 1:  Space : O(n)  Time : O(n)
+        //  k = k % nums.length;
+        // if(k==0 || nums.length == 0 || nums.length == 1){
+        //     return;
+        // }
+        // int ans[] = new int[nums.length];
+        // for(int i=0; i<k; i++){
+        //     ans[i] = nums[nums.length-k+i];
+        // }
+        // for(int i=k; i<nums.length; i++){
+        //     ans[i] = nums[i-k];
+        // }
+        // for(int i=0; i<ans.length; i++){
+        //     nums[i] = ans[i];
+        // }
+        
+        // Method 2: Bubble Sort: Time limit executed Error
+        // k = k % nums.length;
+        // if(k==0 || nums.length == 0 || nums.length == 1){
+        //     return;
+        // }
+        // for(int i=0; i<k; i++){
+        //     for(int j=nums.length-1; j>0; j--){
+        //         int temp = nums[j];
+        //         nums[j] = nums[j-1];
+        //         nums[j-1] = temp;
+        //     }
+        // }
+        
+        // Method 3 : 
+        k = k%nums.length;
         if(k==0 || nums.length == 0 || nums.length == 1){
             return;
         }
-        int ans[] = new int[nums.length];
-        for(int i=0; i<k; i++){
-            ans[i] = nums[nums.length-k+i];
-        }
-        for(int i=k; i<nums.length; i++){
-            ans[i] = nums[i-k];
-        }
-        for(int i=0; i<ans.length; i++){
-            nums[i] = ans[i];
-        }
+        int a = nums.length - k;
+        reverse(nums, 0, a-1);
+        reverse(nums, a, nums.length-1);
+        reverse(nums, 0, nums.length-1);
         
+       
     }    
+     public static void reverse(int[] nums, int s, int e){
+            while(s<e){
+                int temp = nums[e];
+                nums[e] = nums[s];
+                nums[s] = temp;
+                s++;
+                e--;
+            }
+        }
 }
