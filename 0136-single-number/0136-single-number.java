d6a3adb3ad15f114aa3593aeb71b171c8da0b3cf
Vedant Joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2025-02-03 00:03:46 +0530

    Time: 319 ms (6.4%), Space: 46.1 MB (70.83%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,36 +1,50 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        
-        for(int i=0; i<nums.length; i++){
-            boolean flag = false;
-            for(int j=0; j<nums.length; j++){
-                if(i!=j && nums[i]==nums[j]){
-                    flag = true;
-                    break;
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;
+                    arr[j] = -1;
                 }
             }
-            if(flag == false){
-                return nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != -1) {
+                return arr[i];
             }
         }
-        return 0;
-        
+        return -1;
+
+        // for(int i=0; i<nums.length; i++){
+        // boolean flag = false;
+        // for(int j=0; j<nums.length; j++){
+        // if(i!=j && nums[i]==nums[j]){
+        // flag = true;
+        // break;
+        // }
+        // }
+        // if(flag == false){
+        // return nums[i];
+        // }
+        // }
+        // return 0;
+
         // Map<Integer, Integer> map = new HashMap<>();// value and count
         // for(int i=0; i<nums.length; i++){
-        //     if(map.containsKey(nums[i])){
-        //         map.put(nums[i], map.get(nums[i])+1);
-        //     }
-        //     else{
-        //         map.put(nums[i], 1);
-        //     }
+        // if(map.containsKey(nums[i])){
+        // map.put(nums[i], map.get(nums[i])+1);
+        // }
+        // else{
+        // map.put(nums[i], 1);
+        // }
         // }
         // for(Integer ele: map.keySet()){
-        //     if(map.get(ele)==1){
-        //         return ele;
-        //     }
+        // if(map.get(ele)==1){
+        // return ele;
+        // }
         // }
         // return -1;
-        
-        
+
     }
 }
