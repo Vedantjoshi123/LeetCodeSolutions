50826c1225f3f0a388414726649e0f5008ea7aa4
Vedant Joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2025-03-02 19:28:48 +0530

    Time: 1 ms (83.8%), Space: 41.3 MB (23.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,14 +1,52 @@
 class Solution {
     public int reverse(int x) {
-        int rev = 0;
-        while(x!=0){
-            int rem = x%10;
-            x = x/10;
-            if(rev>0 && rev>(Integer.MAX_VALUE - rem) / 10 || rev<0 && rev<(Integer.MIN_VALUE - rem) / 10 ){
+        
+        int ans = 0;
+        int num = Math.abs(x);
+        while(num>0){
+            int rem = num%10;
+            if((ans>(Integer.MAX_VALUE - rem) / 10)){
                 return 0;
             }
-            rev = rev*10 + rem;
+            ans = ans*10 + rem;
+            num /= 10;
         }
-        return rev;
+        if(x>0)
+        return ans; 
+        else
+        return -ans;
+
+
+
+        // int ans = 0;
+        // if(x<0){
+        //     int num = Math.abs(x);
+        //     while(num>0){
+        //         int rem = num%10;
+        //         ans = ans*10 + rem;
+        //         num /= 10;
+        //     }
+        //     return -(ans); 
+        // }
+        // else{
+        //    while(x>0){
+        //         int rem = x%10;
+        //         ans = ans*10 + rem;
+        //         x /= 10;
+        //     }
+        //     return ans; 
+        // }
+        
+
+        // int rev = 0;
+        // while(x!=0){
+        //     int rem = x%10;
+        //     x = x/10;
+        //     if(rev>0 && rev>(Integer.MAX_VALUE - rem) / 10 || rev<0 && rev<(Integer.MIN_VALUE - rem) / 10 ){
+        //         return 0;
+        //     }
+        //     rev = rev*10 + rem;
+        // }
+        // return rev;
     }
 }
