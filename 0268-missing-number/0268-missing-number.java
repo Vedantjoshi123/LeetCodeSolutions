353ff76031cdd7bf9ecd1b8ae34a2de11c7cf167
Vedant Joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2025-06-01 12:16:40 +0530

    Time: 0 ms (100%), Space: 45.4 MB (44.27%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,35 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i;
-        if(nums[0] != 0) return 0;
-        for(i=0;i<nums.length-1; i++){
-            if(nums[i]+1 != nums[i+1]){
-                return nums[i]+1;
-            }
+        int sum = nums.length * (nums.length + 1) / 2;
+        int sum2 = 0;
+        for(int i=0; i<nums.length; i++){
+            sum2 += nums[i];
         }
-        return nums[i]+1;
+        return (sum-sum2);
+
+
+    //  TC = O(n)    SC = O(n)
+        // int[] result = new int[nums.length+2];
+        // for(int i=0; i<nums.length; i++){
+        //     result[nums[i]] = 1;
+        // }
+        // for(int i=0; i<result.length; i++){
+        //     if(result[i] != 1){
+        //         return i;
+        //     }
+        // }
+        // return 0;
+
+        
+        // Arrays.sort(nums);
+        // int i;
+        // if(nums[0] != 0) return 0;
+        // for(i=0;i<nums.length-1; i++){
+        //     if(nums[i]+1 != nums[i+1]){
+        //         return nums[i]+1;
+        //     }
+        // }
+        // return nums[i]+1;
 
         // optimized: 
         // int xor = 0, i = 0;
